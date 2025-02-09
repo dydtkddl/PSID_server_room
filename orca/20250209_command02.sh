2aa1e89fab978cfbf5a1fd6ccea7c19dfc529941
dydtkddl <dydtkddhkdwk@naver.com>
2025-02-09 16:35:41 +0900

    git push

---
+++ orca/20250209_command02.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+orca_bin="/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca"
+
+$orca_bin /home/ys/Research/PSID_server_room/orca/4nitroaniline_3/4nitroaniline_3.inp > \
+          /home/ys/Research/PSID_server_room/orca/4nitroaniline_3/4nitroaniline_3.out
+ 
+$orca_bin /home/ys/Research/PSID_server_room/orca/ABN_3/ABN_3.inp > \
+          /home/ys/Research/PSID_server_room/orca/ABN_3/ABN_3.out
+ 
+$orca_bin /home/ys/Research/PSID_server_room/orca/aniline_3/aniline_3.inp > \
+          /home/ys/Research/PSID_server_room/orca/aniline_3/aniline_3.out
+ 
+$orca_bin /home/ys/Research/PSID_server_room/orca/p_toluidine_3/p_toluidine_3.inp > \
+          /home/ys/Research/PSID_server_room/orca/p_toluidine_3/p_toluidine_3.out
+ 
+$orca_bin /home/ys/Research/PSID_server_room/orca/water_3/water_3.inp > \
+          /home/ys/Research/PSID_server_room/orca/water_3/water_3.out
+ 
+$orca_bin /home/ys/Research/PSID_server_room/orca/zinc_hydrate_2/zinc_hydrate_2.inp > \
+          /home/ys/Research/PSID_server_room/orca/zinc_hydrate_2/zinc_hydrate_2.out
+ 
+$orca_bin /home/ys/Research/PSID_server_room/orca/honokiol_3/honokiol_3.inp > \
+          /home/ys/Research/PSID_server_room/orca/honokiol_3/honokiol_3.out
+ 
