d6921727cff5bd2d7557c2d498fd593f20c32e8a
dydtkddl <dydtkddhkdwk@naver.com>
2025-02-25 17:32:13 +0900

    git push

---
+++ orca/20250225command2.sh
@@ -0,0 +1,11 @@
+cd c60malonicester_o2_2_option3
+cd run_20
+/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca c60_o2_20.inp > c60_o2_20.out
+cd ..
+cd ..
+cd honokiol_2_option3 
+/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_2.inp > honokiol_2.out
+cd ..
+cd superoxideradical_2_option3 
+/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca O2.inp > O2.out
+cd ..
---
+++ orca/20250225command4.sh
@@ -0,0 +1,7 @@
+cd honokiol_o2_2_option3 
+cd run_11 
+/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_o2_11.inp > honokiol_o2_11.out
+cd ..
+cd run_19 
+/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_o2_19.inp> honokiol_o2_19.out
+cd ..
