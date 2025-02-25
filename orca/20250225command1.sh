b50bdc7f6182b117ae5f99305a39052c4f43dceb
dydtkddl <dydtkddhkdwk@naver.com>
2025-02-25 17:33:38 +0900

    git push

--- orca/20250225command1.sh
+++ orca/20250225command1.sh
@@ -1,10 +1,10 @@
 cd c60malonicester_2_option3 
-/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca c60malonicester.inp > c60malonicester.out
+/home/yongsang/orca_6_0_1_linux_x86-64_shared_openmpi416/orca c60malonicester.inp > c60malonicester.out
 cd ..
 
 cd c60malonicester_o2_2_option3
 cd run_17
-/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca c60_o2_17.inp > c60_o2_17.out
+/home/yongsang/orca_6_0_1_linux_x86-64_shared_openmpi416/orca c60_o2_17.inp > c60_o2_17.out
 cd ..
 
 
--- orca/20250225command3.sh
+++ orca/20250225command3.sh
@@ -1,9 +1,9 @@
 cd honokiol_o2_2_option3 
 cd run_4 
-/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_o2_4.inp > honokiol_o2_4.out
+/home/yongsang/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_o2_4.inp > honokiol_o2_4.out
 cd ..
 cd run_9 
-/home/ys/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_o2_9.inp > honokiol_o2_9.out
+/home/yongsang/orca_6_0_1_linux_x86-64_shared_openmpi416/orca honokiol_o2_9.inp > honokiol_o2_9.out
 cd ..
 
 
